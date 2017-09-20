fab342ef7b7dc07452e452eff2bd48f32d255163
Dmitry Tokarev <dmitrytokarev@users.noreply.github.com>
2017-09-20 00:09:22 -0700

    Modified global fixtures: added config fixture and others derived from config. Added dynamic way of getting router ip. (#3)

--- conftest.py
+++ conftest.py
@@ -1,9 +1,30 @@
 import pytest
+from requests import get
 
 
-@pytest.fixture(scope="session",
-                params=(['http://localhost:8000/', '10.0.2.2'], ['http://httpbin.org/', '50.54.242.152']),
+# Options to get router ip:
+# ['https://api.ipify.org', 'http://bot.whatismyipaddress.com', 'http://ipv4bot.whatismyipaddress.com']
+@pytest.fixture(scope='session',
+                params=[
+                    {
+                        'base_url': 'http://localhost:8000/',
+                        'my_ip': '10.0.2.2'
+                    },
+                    {
+                        'base_url': 'http://httpbin.org/',
+                        'my_ip': get('http://bot.whatismyipaddress.com/').text
+                    }
+                ],
                 ids=['dev', 'prod'])
-def base_url(request):
-    print("Create fixture {}".format(request.param))
+def config(request):
     return request.param
+
+
+@pytest.fixture(scope='session')
+def base_url(config):
+    return config['base_url']
+
+
+@pytest.fixture(scope='session')
+def my_ip(config):
+    return config['my_ip']
