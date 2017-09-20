6461143ca11c73d90ae85c40894a07ded3128e05
nicolas13 <nicolas13@MacBook-Pro-nicolas13.local>
2017-09-20 15:16:52 +0300

    api test first attempt

---
+++ test_n13_api.py
@@ -0,0 +1,25 @@
+import requests
+
+
+def test_httpbin_post(config):
+    mydata = [{'name': 'Nikolay'}, 'hello']
+    r = requests.post(config['base_url'] + 'post', json=mydata)
+    assert r.status_code == 200, r.text
+    data = r.json()
+    assert data['json'] == mydata
+
+
+def test_httpbin_ip(config):
+    r = requests.get(config['base_url'] + 'ip')
+    assert r.status_code == 200, r.text
+    data = r.json()
+    assert data == {u'origin': config['my_ip']}
+    assert r.elapsed.total_seconds() < 0.500
+
+
+def test_user_agent(config):
+    r = requests.get(config['base_url'] + 'user-agent')
+    assert r.status_code == 200, r.text
+    data = r.json()
+    assert data == {u'user-agent': 'python-requests/' + str(requests.__version__)}
+    assert r.elapsed.total_seconds() < 0.500
