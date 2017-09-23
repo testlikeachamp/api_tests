14a8ab154ad7dede030295682772b36b70f3e267
Artem Selekhov <a-selehov@yandex.ru>
2017-09-23 22:51:51 +0300

    Add first test

---
+++ test_httpbin_user_agent.py
@@ -0,0 +1,17 @@
+
+from requests import get
+import requests
+
+from conftest import config
+
+
+def test_httpbin_user_agent():
+    r = get(config['base_url'] + 'user-agent')
+    assert r.status_code == 200, r.text
+    assert r.reason == "OK", r.text
+    assert r.json()['user-agent'] == 'python-requests/' + requests.__version__
+    print(r.elapsed.total_seconds())
+    #data = r.json()
+    #assert len(r.json())
+    #print(data)
+    #print(r.reason)
