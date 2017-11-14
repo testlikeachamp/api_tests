85be6eab967460fcecd345954c29884f7c185c70
nicolas13sochi <nikolay.grishchenko@gmail.com>
2017-11-15 00:33:28 +0300

    From junior to medium test with parametrization

--- test_n13_images.py
+++ test_n13_images.py
@@ -1,56 +1,28 @@
 import filecmp
 import os
+import pytest
 
 
 from requests import get
 
 
-def test_image_png(base_url):
-    r = get(base_url + 'image', headers={'Accept': 'image/png'})
+@pytest.mark.parametrize('test_format,test_image', [
+    ('png', ['pig_png.png', 'downloaded.png']),
+    ('webp', ['wolf_webp', 'downloaded']),
+    ('svg+xml', ['svg_svg.svg', 'downloaded.svg'])
+])
+def test_image_endpoint(base_url, test_format, test_image):
+    r = get(base_url + 'image', headers={'Accept': 'image/'+test_format})
     assert r.status_code == 200
     assert r.reason == 'OK'
     assert r.elapsed.total_seconds() < 1.0
 
-    filename = 'pig_png.png'
+    filename = test_image[0]
     path_to_current_file = os.path.realpath(__file__)
     current_directory = os.path.split(path_to_current_file)[0]
     data_file_path = os.path.join(current_directory, filename)
     assert r.content == open(data_file_path, 'rb').read()
 
-    with open('downloaded.png', 'wb') as f:
+    with open(test_image[1], 'wb') as f:
         f.write(r.content)
-    assert filecmp.cmp('downloaded.png', data_file_path, shallow=False)
-
-
-def test_image_webp(base_url):
-    r = get(base_url + 'image', headers={'Accept': 'image/webp'})
-    assert r.status_code == 200
-    assert r.reason == 'OK'
-    assert r.elapsed.total_seconds() < 1.0
-
-    filename = 'wolf_webp'
-    path_to_current_file = os.path.realpath(__file__)
-    current_directory = os.path.split(path_to_current_file)[0]
-    data_file_path = os.path.join(current_directory, filename)
-    assert r.content == open(data_file_path, 'rb').read()
-
-    with open('downloaded', 'wb') as f:
-        f.write(r.content)
-    assert filecmp.cmp('downloaded', data_file_path, shallow=False)
-
-
-def test_image_svg(base_url):
-    r = get(base_url + 'image', headers={'Accept': 'image/svg+xml'})
-    assert r.status_code == 200
-    assert r.reason == 'OK'
-    assert r.elapsed.total_seconds() < 1.0
-
-    filename = 'svg_svg.svg'
-    path_to_current_file = os.path.realpath(__file__)
-    current_directory = os.path.split(path_to_current_file)[0]
-    data_file_path = os.path.join(current_directory, filename)
-    assert r.content == open(data_file_path, 'rb').read()
-
-    with open('downloaded.svg', 'wb') as f:
-        f.write(r.content)
-    assert filecmp.cmp('downloaded.svg', data_file_path, shallow=True)
+    assert filecmp.cmp(test_image[1], data_file_path, shallow=False)
