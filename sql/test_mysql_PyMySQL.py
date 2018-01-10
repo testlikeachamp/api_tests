e59739c3ffc975139949c7fbb747dcc2a21121c1
Dmitry Tokarev <dm.tokarev@yahoo.com>
2018-01-10 01:22:37 -0800

    cleanup

--- sql/test_mysql_PyMySQL.py
+++ sql/test_mysql_PyMySQL.py
@@ -1,11 +1,4 @@
-# download mysql-connector-python-2.1.7.tar.gz from
-# https://dev.mysql.com/downloads/connector/python/
-# or
-# https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.7.tar.gz
-# will need to create free account or login to your existing account
-
 '''
-scp -P 2222 /Users/dmitrytokarev/Downloads/mysql-connector-python-2.1.7.tar.gz default@localhost:/home/default/
 ssh -p 2222 default@localhost
 python -m venv venv
 cd venv/
@@ -53,6 +46,7 @@ finally:
     connection.close()
 
 
+# our own example
 s1 = '''
 CREATE TABLE Persons (
    PersonID int,
@@ -69,26 +63,3 @@ with connection.cursor() as cursor:
     cursor.execute(s3)
     r = cursor.fetchall()
     assert r == [{'PersonID': 123456, 'LastName': 'Tokarev', 'FirstName': 'Dmitry', 'Address': 'USA', 'City': 'Seattle'}]
-
-
-
-
-# cnx = mysql.connector.connect(user='root', password='default', host='127.0.0.1')
-# cnx.cmd_query('create database test;')
-# cnx.connect(database='test')
-#
-# s1 = '''
-# CREATE TABLE Persons (
-#    PersonID int,
-#    LastName varchar(255),
-#    FirstName varchar(255),
-#    Address varchar(255),
-#    City varchar(255)
-# );'''
-# s2 = "INSERT INTO Persons (PersonID, LastName, FirstName, Address, City) VALUES (123456, 'Tokarev', 'Dmitry', 'USA', 'Seattle');"
-# s3 = "select * from Persons"
-# cursor = cnx.cursor()
-# cursor.execute(s2)
-# cursor.execute(s3)
-# r = cursor.fetchall()
-# assert r == [(123456, 'Tokarev', 'Dmitry', 'USA', 'Seattle')]
