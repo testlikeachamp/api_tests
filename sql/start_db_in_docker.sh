5d84a0c23ca0f08ca9c10a444580bc5d505fb93e
Dmitry Tokarev <dm.tokarev@yahoo.com>
2018-01-11 12:28:15 -0800

    added notes about port forwarding on VM

--- sql/start_db_in_docker.sh
+++ sql/start_db_in_docker.sh
@@ -4,10 +4,10 @@ sudo systemctl enable docker
 # run this command to start docker now
 sudo systemctl start docker
 
-# start MySQL
+# start MySQL - add port forwarding on VM 3306:3306
 docker run -p 3306:3306 --name mysql-1 -e MYSQL_ROOT_PASSWORD=default -d mysql:latest
 
-# start PostgreSQL
+# start PostgreSQL - add port forwarding on VM 5432:5432
 docker run --name my-postgres -e POSTGRES_PASSWORD=default -p 5432:5432 -d postgres
 
 # install python libs for PostgreSQL
